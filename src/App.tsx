171705f78d627779ed29ba936efabf2c42152d5d
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-06-25 08:44:20 +0300

    feat: Add firebase context.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import FirebaseContext from './db/firebase.context';
+import app from './db/firebase.config';
+import { db } from './db/firebase.config';
 
 function App() {
     return (
-        <div className="instagram-clone-inner-wrapper">
-            <h1>Hello world!</h1>
-            <p>This is just a regullar test!</p>
-        </div>
+        <FirebaseContext.Provider value={{ app, db }}>
+            <div className="instagram-clone-inner-wrapper">
+                <h1>Hello world!</h1>
+                <p>This is just a regullar test!</p>
+            </div>
+        </FirebaseContext.Provider>
     );
 }
 
