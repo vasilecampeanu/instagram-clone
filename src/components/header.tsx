f2e2f088c423fadff4958e8c6b08762b6944e25d
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-07-03 01:53:01 +0300

    feat: Add basic post modal.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext, useEffect } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
 import * as ROUTES from '../constants/routes';
 import FirebaseContext from '../db/firebase.context';
 import { Link, useNavigate } from 'react-router-dom';
@@ -9,7 +9,11 @@ import instagram_logo_hand_written from '../assets/images/instagram_logo_hand_wr
 import { FirebaseApp } from 'firebase/app';
 import { NavigateFunction } from 'react-router-dom';
 
+import Modal from './modal';
+
 const Header: FC<{}> = () => {
+    const [show, setShow] = useState(false);
+
     const firebase: FirebaseApp | undefined = useContext<FirebaseApp | undefined>(FirebaseContext);
     
     const user: User | undefined = useContext<User | undefined>(UserContext);
@@ -46,14 +50,11 @@ const Header: FC<{}> = () => {
                             <div className="new-post">
                                 <button
                                     className="new-post-btn"
-                                    onClick={() => {
-                                        console.log("New post!");
-                                    }}
+                                    onClick={() => setShow(true)}
                                 >
                                     <svg aria-label="New post" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="6.545" x2="17.455" y1="12.001" y2="12.001"></line><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="12.003" x2="12.003" y1="6.545" y2="17.455"></line></svg>
                                 </button>
-                                <div className="modal">
-                                </div>
+                                <Modal show={show} onClose={() => setShow(false)} />
                             </div>
                             <button
                                 className="btnLogOut"
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,8 +1,46 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
-const Modal:FC<{}> = () => {
-    return(
-        <>
-        </>
+interface Props {
+    show: boolean;
+    onClose: () => void
+}
+
+const Modal:FC<Props> = ({ show, onClose }) => {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    if (!show) {
+        return null;
+    }
+
+    return (
+        <div className="modal">
+            <button onClick={onClose} className="btnClose">Close</button>
+            <div className="modal-content">
+                <div className="modal-header">
+                    Create new post
+                </div>
+                <div className="modal-body">
+                    {selectedImage ? (
+                        <div>
+                            <img alt="not fount" width={"250px"} src={URL.createObjectURL(selectedImage)} />
+                            <br />
+                            <button onClick={()=>setSelectedImage(null)}>Remove</button>
+                            <button>Post</button>
+                        </div>
+                    ) : (
+                        <input
+                            type="file"
+                            name="myImage"
+                            onChange={(event:any) => {
+                                console.log(event.target.files[0]);
+                                setSelectedImage(event.target.files[0]);
+                            }}
+                        />
+                    )}
+                </div>
+            </div>
+        </div>
     )
 }
+
+export default Modal;
