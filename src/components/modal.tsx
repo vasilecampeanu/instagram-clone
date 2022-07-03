ce3cf3d2dddcb04a5d75bf6d852de17354a415a7
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-07-04 02:26:09 +0300

    feat: Finish add post component style.

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -28,33 +28,44 @@ const Modal:FC<Props> = ({ show, onClose }) => {
 
     return (
         <div className="modal">
-            <button onClick={onClose} className="btnClose">Close</button>
+            <button onClick={onClose} className="btnClose">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+            </button> 
             <div className="modal-content">
                 <div className="modal-header">
-                    Create new post
+                    <p>Create new post</p>
                 </div>
                 <div className="modal-body">
                     {selectedImage ? (
-                        <div>
+                        <div className="image-selected">
                             <img alt="not fount" width={"250px"} src={URL.createObjectURL(selectedImage)} />
-                            <br />
-                            <button onClick={()=>setSelectedImage(null)}>Remove</button>
-                            <button onClick={() => {
-                                createPost(firebase, userId, photoId, imageName).then(onClose);
-                            }}>Post</button>
+                            <div className="actions">
+                                <button onClick={()=>setSelectedImage(null)}>Remove</button>
+                                <button onClick={() => {
+                                    createPost(firebase, userId, photoId, imageName).then(onClose);
+                                }}>Post</button>
+                            </div>
                         </div>
                     ) : (
-                        <input
-                            type="file"
-                            name="myImage"
-                            onChange={(event:any) => {
-                                console.log(event.target.files[0]);
-                                console.log(event.target.files[0].name);
-                                setImageName(event.target.files[0].name);
-                                console.log(URL.createObjectURL(event.target.files[0]));
-                                setSelectedImage(event.target.files[0]);
-                            }}
-                        />
+                        <>
+                            <svg aria-label="Icon to represent media such as images or videos" color="#262626" fill="#262626" height="77" role="img" viewBox="0 0 97.6 77.3" width="96"><path d="M16.3 24h.3c2.8-.2 4.9-2.6 4.8-5.4-.2-2.8-2.6-4.9-5.4-4.8s-4.9 2.6-4.8 5.4c.1 2.7 2.4 4.8 5.1 4.8zm-2.4-7.2c.5-.6 1.3-1 2.1-1h.2c1.7 0 3.1 1.4 3.1 3.1 0 1.7-1.4 3.1-3.1 3.1-1.7 0-3.1-1.4-3.1-3.1 0-.8.3-1.5.8-2.1z" fill="currentColor"></path><path d="M84.7 18.4L58 16.9l-.2-3c-.3-5.7-5.2-10.1-11-9.8L12.9 6c-5.7.3-10.1 5.3-9.8 11L5 51v.8c.7 5.2 5.1 9.1 10.3 9.1h.6l21.7-1.2v.6c-.3 5.7 4 10.7 9.8 11l34 2h.6c5.5 0 10.1-4.3 10.4-9.8l2-34c.4-5.8-4-10.7-9.7-11.1zM7.2 10.8C8.7 9.1 10.8 8.1 13 8l34-1.9c4.6-.3 8.6 3.3 8.9 7.9l.2 2.8-5.3-.3c-5.7-.3-10.7 4-11 9.8l-.6 9.5-9.5 10.7c-.2.3-.6.4-1 .5-.4 0-.7-.1-1-.4l-7.8-7c-1.4-1.3-3.5-1.1-4.8.3L7 49 5.2 17c-.2-2.3.6-4.5 2-6.2zm8.7 48c-4.3.2-8.1-2.8-8.8-7.1l9.4-10.5c.2-.3.6-.4 1-.5.4 0 .7.1 1 .4l7.8 7c.7.6 1.6.9 2.5.9.9 0 1.7-.5 2.3-1.1l7.8-8.8-1.1 18.6-21.9 1.1zm76.5-29.5l-2 34c-.3 4.6-4.3 8.2-8.9 7.9l-34-2c-4.6-.3-8.2-4.3-7.9-8.9l2-34c.3-4.4 3.9-7.9 8.4-7.9h.5l34 2c4.7.3 8.2 4.3 7.9 8.9z" fill="currentColor"></path><path d="M78.2 41.6L61.3 30.5c-2.1-1.4-4.9-.8-6.2 1.3-.4.7-.7 1.4-.7 2.2l-1.2 20.1c-.1 2.5 1.7 4.6 4.2 4.8h.3c.7 0 1.4-.2 2-.5l18-9c2.2-1.1 3.1-3.8 2-6-.4-.7-.9-1.3-1.5-1.8zm-1.4 6l-18 9c-.4.2-.8.3-1.3.3-.4 0-.9-.2-1.2-.4-.7-.5-1.2-1.3-1.1-2.2l1.2-20.1c.1-.9.6-1.7 1.4-2.1.8-.4 1.7-.3 2.5.1L77 43.3c1.2.8 1.5 2.3.7 3.4-.2.4-.5.7-.9.9z" fill="currentColor"></path></svg>
+                            <label htmlFor="upload-photo">Select from computer</label>
+                            <input
+                                type="file"
+                                name="myPhoto"
+                                id="upload-photo"
+                                onChange={(event:any) => {
+                                    console.log(event.target.files[0]);
+                                    console.log(event.target.files[0].name);
+                                    setImageName(event.target.files[0].name);
+                                    console.log(URL.createObjectURL(event.target.files[0]));
+                                    setSelectedImage(event.target.files[0]);
+                                }}
+                            />
+                        </>
                     )}
                 </div>
             </div>
